bfe3a852256f1d927f21a86e5b9c38158fa6cda0
Johan Björk <johanimon@gmail.com>
2019-04-27 15:52:55 +0200

    Initial commit

---
+++ src/Machine.kt
@@ -0,0 +1,5 @@
+import java.io.InputStream
+
+class Machine(val rom: InputStream) {
+
+}
---
+++ tests/MachineTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class MachineTest {
+    @Test
+    fun bootShouldDoSomething() {
+        assertTrue(true)
+    }
+}
