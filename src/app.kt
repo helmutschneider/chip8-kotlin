32b30f08170c3d402b35c434e8e64a6cec6538b4
Johan Björk <johanimon@gmail.com>
2019-07-12 12:28:05 +0200

    Allow loading other roms

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,15 @@
-fun main() {
-    val rom = Machine::class.java.getResourceAsStream("Space Invaders [David Winter].ch8")
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main(argv: Array<String>) {
+    val rom = if (argv.isNotEmpty()) {
+        Files.newInputStream(
+            Paths.get(argv[0])
+        )
+    } else {
+        Machine::class.java.getResourceAsStream("Space Invaders [David Winter].ch8")
+    }
+
     val machine = Machine(rom, SwingInputOutput())
 
     machine.boot()
